1ce4a75aa1e5ff8282c28713e980a9f79c240407
VickyBondarenko <quikyv@gmail.com>
2023-02-13 20:32:08 +0100

    add pagination

--- src/index.js
+++ src/index.js
@@ -6,18 +6,33 @@ const gallery = document.querySelector('.gallery');
 const loadButton = document.querySelector('.load-more');
 // console.log(loadButton);
 
+const perPage = 8;
+let page = 1;
 form.addEventListener('submit', onSerch);
 
 function onSerch(event) {
   event.preventDefault();
   gallery.innerHTML = '';
+  // let page = 1;
+
   const keyWord = form.searchQuery.value.trim();
-  console.log(keyWord);
+
   getResponse(keyWord)
     .then(response => {
-      Notify.success(`Hooray! We found ${response.totalHits} images.`);
-      let page = '2';
-      renderGallery(response.hits, page);
+      const totalPages = Math.ceil(response.totalHits / perPage);
+      console.log(totalPages);
+      if (page < 2) {
+        Notify.success(`Hooray! We found ${response.totalHits} images.`);
+      }
+
+      renderGallery(response.hits);
+      page += 1;
+      if (page > totalPages) {
+        Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+        loadButton.classList.add('is-hidden');
+      }
       console.log(response);
     })
     .catch(error => {
@@ -60,14 +75,41 @@ function renderGallery(fotos) {
   gallery.insertAdjacentHTML('beforeend', markupGallery);
   loadButton.classList.remove('is-hidden');
 }
+
 loadButton.addEventListener('click', onSerch);
+
+// function onLoad() {
+//   const keyWord = form.searchQuery.value.trim();
+
+//   getResponse(keyWord)
+//     .then(response => {
+//       const totalPages = Math.ceil(response.totalHits / perPage);
+//       console.log(totalPages);
+//       if (page < 2) {
+//         Notify.success(`Hooray! We found ${response.totalHits} images.`);
+//       }
+
+//       renderGallery(response.hits);
+//       page += 1;
+//       if (page > totalPages) {
+//         Notify.warning(
+//           "We're sorry, but you've reached the end of search results."
+//         );
+//         loadButton.classList.add('is-hidden');
+//       }
+//       console.log(response);
+//     })
+//     .catch(error => {
+//       Notify.failure(error.message);
+//     });
+// }
+
 function getResponse(name) {
   const API_KEY = `33587680-716f90511d290523ac71bcbd3`;
-  let page = '1';
 
   return axios
     .get(
-      `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=4`
+      `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
     )
     .then(response => {
       if (response.data.totalHits === 0) {
@@ -75,7 +117,7 @@ function getResponse(name) {
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
-      page += 1;
+
       return response.data;
     });
 }
